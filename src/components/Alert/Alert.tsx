274de1d28c2df006a7286b35ace9ab25a211aff5
B. Gunnarsson <bgunnarsson@VTTV-KINGBINM5.local>
2026-04-27 16:42:53 +0000

    update

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -17,21 +17,13 @@ export const Alert = ({ as: headingLevel, heading, description, children, ...pro
 export interface AlertTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
-export const AlertTitle = ({ as: Tag = 'h5', ...props }: AlertTitleProps) => (
-  <Tag data-part="title" {...props} />
-)
+export const AlertTitle = ({ as: Tag = 'h5', ...props }: AlertTitleProps) => <Tag data-part="title" {...props} />
 
 export interface AlertDescriptionProps extends React.HTMLAttributes<HTMLParagraphElement> {}
-export const AlertDescription = (props: AlertDescriptionProps) => (
-  <p data-part="description" {...props} />
-)
+export const AlertDescription = (props: AlertDescriptionProps) => <p data-part="description" {...props} />
 
 export interface AlertIconProps extends React.HTMLAttributes<HTMLSpanElement> {}
-export const AlertIcon = (props: AlertIconProps) => (
-  <span aria-hidden="true" data-part="icon" {...props} />
-)
+export const AlertIcon = (props: AlertIconProps) => <span aria-hidden="true" data-part="icon" {...props} />
 
 export interface AlertActionsProps extends React.HTMLAttributes<HTMLDivElement> {}
-export const AlertActions = (props: AlertActionsProps) => (
-  <div data-part="actions" {...props} />
-)
+export const AlertActions = (props: AlertActionsProps) => <div data-part="actions" {...props} />
